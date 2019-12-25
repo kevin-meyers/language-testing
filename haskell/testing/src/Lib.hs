5ef9875187b3fec18548a7271d65c337039ddf1b
Kevin <kevinm1776@gmail.com>
2019-12-25 00:50:07 -0700

    Added testing environment.

---
+++ haskell/testing/src/Lib.hs
@@ -0,0 +1,15 @@
+module Lib
+    ( someFunc
+    ) where
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
+
+doubleMe :: Int -> Int
+doubleMe x = 2*x
+
+doubleUs :: Int -> Int -> Int
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber :: Int -> Int
+doubleSmallNumber x = if x > 100 then x else doubleMe x
