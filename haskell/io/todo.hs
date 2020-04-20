b3d0b7aeaa8cc98bb923003bada9e0cff032be50
Kevin <kevinm1776@gmail.com>
2020-04-20 17:14:43 -0400

    First feature full io program

---
+++ haskell/io/todo.hs
@@ -0,0 +1,47 @@
+import Control.Exception (bracketOnError)
+import Data.List (delete, lines, unlines, zipWith)
+import System.Directory (removeFile, renameFile)
+import System.Environment (getArgs)
+import System.IO (appendFile, hClose, hPutStr, openTempFile, readFile)
+
+dispatch :: String -> [String] -> IO ()
+dispatch "add" = add
+dispatch "view" = view
+dispatch "remove" = remove
+
+main :: IO ()
+main = do
+  (command:argList) <- getArgs
+  dispatch command argList
+
+add :: [String] -> IO ()
+add [fileName, toDoItem] = appendFile fileName $ toDoItem ++ "\n"
+
+view :: [String] -> IO ()
+view [fileName] = do
+  contents <- readFile fileName
+  let todoTasks = lines contents
+      numberedTasks =
+        zipWith (\n line -> show n ++ " - " ++ line) [0 ..] todoTasks
+  putStr $ unlines numberedTasks
+
+remove :: [String] -> IO ()
+remove [fileName, index] = do
+  contents <- readFile fileName
+  let todoTasks = lines contents
+      numberedTasks =
+        zipWith (\n line -> show n ++ " - " ++ line) [0 ..] todoTasks
+  putStrLn "These are your TODO items:"
+  let number = read index
+      newTodoItems = unlines $ delete (todoTasks !! number) todoTasks
+  mapM_ putStrLn $ lines newTodoItems
+  bracketOnError
+    (openTempFile "." "temp")
+    (\(tempName, tempHandle) -> do
+       hClose tempHandle
+       removeFile tempName)
+    (\(tempName, tempHandle) -> do
+       hPutStr tempHandle newTodoItems
+       hClose tempHandle
+       removeFile fileName
+       renameFile tempName fileName)
